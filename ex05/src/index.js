78729587bc0a31fd7c5a3424f7888c1e5c1e70ef
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-20 10:20:17 +0100

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,30 @@
+// Only change code below this line
+function myPetsFunction(pets) {
+    myPetsArray = [
+        {
+            animalType: "Dog",
+            name: [
+                "Pujdo"
+            ]
+        },
+        {
+            animalType: "Cat",
+            category: [
+                "Maca"
+            ]
+        },
+        {
+            animalType: "Bird",
+            name: [
+                "Tweety"
+            ]
+        },
+    ];
+    myPetsArray[1].category[0];
+    
+}
+// Only change code above this line
 
+console.log(myPetsFunction(pets)); // Change this line
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
