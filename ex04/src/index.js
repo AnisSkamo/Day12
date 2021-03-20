f15104982bf4566c535fa1901c31761ca6bae7a3
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-20 09:50:40 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,14 @@
+// Only change code below this line
+function sumFibonacci(n) {
+    return n < 1 ? 0
+    : n<= 2 ? 1
+    : sumFibonacci (n - 1) + sumFibonacci(n - 2);
+}
+// Only change code above this line
 
+console.log(sumFibonacci(1));
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
+module.exports = sumFibonacci;
